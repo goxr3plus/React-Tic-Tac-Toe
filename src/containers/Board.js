8c7d7ac47d6513a42d06a6cb4d109b4b39759800
Kentros Alexandros <Alexandros.KENTROS@intrasoft-intl.com>
2019-10-07 15:16:58 +0300

    Implemented Winner

--- src/containers/Board.js
+++ src/containers/Board.js
@@ -11,9 +11,14 @@ export default class Board extends Component {
     };
   }
 
+  // ------------------------- ACTION METHODS -------------------------//
+
   handleClick = i => {
     /* Shallow copy the previous state */
     const squares = [...this.state.squares]; //or  = Array.from(this.state.squares)
+
+    /* Check if we have winner */
+    if (this.calculateWinner(squares) || squares[i]) return;
 
     /* Determine who is next */
     squares[i] = this.state.xIsNext ? 'X' : 'O';
@@ -22,14 +27,34 @@ export default class Board extends Component {
     this.setState(state => ({ squares, xIsNext: !state.xIsNext }));
   };
 
+  // ------------------------- GENERIC METHODS -------------------------//
+
   renderSquare(i) {
     return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
   }
 
+  calculateWinner = squares => {
+    const lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
+  // ------------------------- RENDER -------------------------//
 
   render() {
-    const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+    const winner = this.calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
